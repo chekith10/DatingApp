e0c46b5add815eb6add235d0278aee033e171989
chekith10 <schekith@gmail.com>
2023-05-26 16:30:06 -0400

    End of Section 13

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -6,7 +6,20 @@ namespace API.Extensions
     {
         public static string GetUsername(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            var claim = user.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null && int.TryParse(claim.Value, out int userId))
+            {
+                return userId;
+            }
+
+            // Return a default value or throw an exception, depending on your requirements
+            throw new InvalidOperationException("User identifier not found or is not a valid integer.");
         }
     }
 }
